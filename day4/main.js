8c02f690cb693aa3ce82fdab25cf42980143d9cd
yonatan.b <yonatanbenezra1@gmail.com>
2020-08-13 10:31:48 +0300

    Day4

---
+++ day4/main.js
@@ -0,0 +1,143 @@
+// Hi Danny and Yonatan! This is day4 excersises
+
+// const add = function (x, y) {
+//     const sum = x + y
+//     return sum
+//     console.log("Returned " + sum)
+// }
+
+// add(1, 2)
+// 
+// const getInterest = function(money){
+// return money * 0.2
+//   }
+
+//   const getDebt = function(money){
+//     const interest = getInterest(money)
+//     return money + interest
+//   }
+
+//   const borrowedMoney = 100
+//   const debt = getDebt(borrowedMoney)
+//   console.log(debt) // prints 120
+
+// spotcheck
+
+
+// const person = {
+//     name: "Julius",
+//     eat: function(food) {
+//       console.log("I like" + food)
+//     }
+//   }
+// person.eat(" cheese toast")
+
+//END
+
+
+//FUNCTIONS EX1DAY4
+
+// const calcAge = function(x, y) {
+//     return x-y
+// }
+
+// const age = calcAge(2017, 1989)
+// console.log(age)
+
+//END
+
+
+//FUNCTIONS EX2DAY4
+
+// const posAge = function(currentYear, bornIn) {
+//     return currentYear-bornIn
+// }
+
+// const age = posAge(2018, 2015)
+// console.log("you are either " +age+ " or " +(age-1))
+
+//END
+
+
+//FUNCTIONS EX3DAY4
+
+// const isEven = function(num) {
+//     return num%2 === 0
+// }
+// const number =isEven(14)
+// console.log(number)
+
+//END
+
+
+// FUNCTIONS EX4DAY4
+
+// const arr = [12, 4, 34, 234, 1, 3, 9]
+
+// const isEven = function (num) {
+//     if (num % 2 === 0)
+//     console.log(num)
+// }
+// for (a of arr) {
+//     isEven(a)
+// }
+
+//END
+
+//FUNCTIONS EX5DAY4
+
+// const array = [1 , 2, 3, 4, 5, 6, 7, 8, 9]
+// const func = function (arr, num) {
+//     for (i of arr) {
+//     } return i === num
+// }
+// const x = func(array,6)
+// console.log(x)
+
+// END
+
+//FUNCTIONS EX6DAY4
+
+// const calculator = {
+//     add: function (x, y) {
+//         return x + y
+//     },
+//     subtract: function (x, y) {
+//         return x - y
+//     }
+// }
+
+// const result1 = calculator.add(20, 1)
+// const result2 = calculator.subtract(30, 9)
+
+// console.log(calculator.add(result1, result2))
+
+// END
+
+// FUNCTIONS EX7DAY4
+
+// const increaseByNameLength = function (money, name) {
+//     return money*name.length
+// }
+// const makeRegal = function (name) {
+//     return "His Royal Highness, " +name
+// }
+// const turnToKing = function (name, money) {
+//     name = name.toUpperCase()
+//     money = increaseByNameLength(money, name)
+//     name = makeRegal(name)
+
+//     console.log(name + " has " + money + " gold coins")
+// }
+
+// turnToKing("martin luther", 100) // should print "His Royal Highness, MARTIN LUTHER has 1300 gold coins"
+
+// END
+
+// FUNCTIONS EX8DAY4
+
+
+
+
+
+
